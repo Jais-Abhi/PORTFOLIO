646a52a0bb583b15ac0e74e41a6ba48b6e2e26f4
Abhishek jaiswal <abhijaiswal7479@gmail.com>
2026-01-22 20:04:58 +0530

    modifief

--- src/Sections/Navbar.js
+++ src/Sections/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div className='flex items-center justify-center ' >
         <div 
-        className='flex items-center justify-center fixed top-4 z-10 bg-white/10 rounded-full' >
+        className='flex items-center justify-center fixed top-4 z-10 bg-white/10 backdrop-blur rounded-full' >
             <Link href="/" className='nav-item bg-white text-black ' >Home</Link>
             <Link href="/"  className='nav-item'>Projects</Link>
             <Link href="/" className='nav-item' >About</Link>
