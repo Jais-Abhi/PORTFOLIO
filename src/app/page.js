9b15ac49e0063cb7b0c6491c1ffbb8ff0c3eb69a
Abhishek jaiswal <abhijaiswal7479@gmail.com>
2026-01-22 20:10:20 +0530

    connected

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,14 @@ import Navbar from '@/Sections/Navbar'
 import React from 'react'
 import "./globals.css"
 import Hero from '@/Sections/Hero'
+import ProjectSection from '@/Sections/ProjectSection'
 const page = () => {
   return (
     <div >
       
       <Navbar/>
       <Hero/>
+      <ProjectSection/>
     </div>
   )
 }
