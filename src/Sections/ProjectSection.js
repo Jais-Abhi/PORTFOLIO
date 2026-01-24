33c0d2989751586bf8f40b5570f97e7bd1bc6928
Abhishek jaiswal <abhijaiswal7479@gmail.com>
2026-01-24 23:39:04 +0530

    project-section : left section of card

--- src/Sections/ProjectSection.js
+++ src/Sections/ProjectSection.js
@@ -4,33 +4,33 @@ const ProjectSection = () => {
   return (
     <div className='flex flex-col items-center w-full' >
       <div className='text-4xl font-bold text-center' >Featured Projects</div>
-      <div className='text-xl text-white/50 mt-4 text-center' >
+      <div className='text-xl text-white/50 mt-4 text-center mb-8' >
       See how i transform concepts into engaging digital experiences 
       </div>
       <div className='w-[80%] flex flex-col gap-16 justify-center items-center ' >
         {projects.map((project)=>{
           return (
             <div key={project.id} 
-            className=' w-full flex border-2 border-white/50 rounded-2xl h-[25rem] '
+            className=' w-full flex border-2 bg-white/10 border-white/50 rounded-2xl h-[25rem] '
             >
               <div className='w-1/2  pt-8 pl-16 pr-16 ' >
                 <div className='text-center font-bold text-2xl' >
                   {project.title}
-                  <hr className=' text-center' />
+                  <hr className=' text-center mb-6 mt-6 text-white/40' />
                 </div>
-                <div>
+                <div className=' flex flex-col gap-6 mb-8 text-[1rem] ' >
                   {project.description.map((point,index)=>{
                     return (
                       <div key={index} > {point} </div>
                     )
                   })}
                 </div> 
-                <div className='flex gap-8 text-[1.3rem] font-semibold  text-black ' >
+                <div className='flex gap-8 text-[1.3rem] font-semibold  text-black mt-12 ' >
                   <button className=' bg-white cursor-pointer rounded-lg px-8 py-2 ' >Github</button>
                   <button className=' bg-white cursor-pointer rounded-lg px-8 py-2 ' > Visit live site</button>
                 </div>
               </div>
-              <div className='w-1/2' >
+              <div className='w-1/2 bg-red-400 ' >
                 <img src={project.image} alt={project.title} />
               </div>
             </div>
