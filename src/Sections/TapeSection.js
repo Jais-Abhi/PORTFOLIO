fe4f23d94a4c9900d628f8dcd685c63e3d72206a
Abhishek jaiswal <abhijaiswal7479@gmail.com>
2026-01-25 21:51:51 +0530

    Tape-section : added line

--- src/Sections/TapeSection.js
+++ src/Sections/TapeSection.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import { BsStars } from "react-icons/bs";
-
+import { data } from '@/Components/TapeData';
 const TapeSection = () => {
   return (
-    <div>
-        <div></div>
+    <div className='mt-16 mb-16' >
+        <div className='flex bg-linear-to-r from-green-400 to-blue-400 p-4 lg:gap-8 gap-2 overflow-hidden items-center justify-center ' >
+            {
+                data.items.map((item,index)=>{
+                    return(
+                        <div 
+                        className='flex text-black font-extrabold text-[1rem]'
+                        > <BsStars className='lg:mr-8 mr-2' /> {item}</div>
+                    )
+                })
+            }
+        </div>
     </div>
   )
 }
