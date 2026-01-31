0dc51659750a7ffb749dd7d01391ee4d9daf2160
Abhishek jaiswal <abhijaiswal7479@gmail.com>
2026-01-31 22:00:31 +0530

    About Section : styled hobbie button as component

---
+++ src/Components/HobbieButton.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const HobbieButton = ({hobbie,icon}) => {
+  return (
+    <div className=' text-black cursor-po font-bold bg-linear-to-r from-blue-400 to-emerald-400 flex px-6 py-2 mx-6 rounded-full ' >
+        <div className='flex gap-2' >
+            <div>
+                {hobbie}
+            </div>
+            {icon}
+        </div>
+    </div>
+  )
+}
+
+export default HobbieButton
