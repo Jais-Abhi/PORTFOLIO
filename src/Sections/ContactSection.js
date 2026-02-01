be35f1e183c37eabe731a91e1d5a846dd3b91792
Abhishek jaiswal <abhijaiswal7479@gmail.com>
2026-02-01 13:06:30 +0530

    Contact-section : initialize

--- src/Sections/ContactSection.js
+++ src/Sections/ContactSection.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import { MdArrowOutward } from "react-icons/md";
 
 const ContactSection = () => {
   return (
-    <div>ContactSection</div>
+    <div></div>
   )
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import Hero from '@/Sections/Hero'
 import ProjectSection from '@/Sections/ProjectSection'
 import TapeSection from '@/Sections/TapeSection'
 import AboutSection from '@/Sections/AboutSection'
+import ContactSection from '@/Sections/ContactSection'
 const page = () => {
   return (
     <div >
@@ -14,6 +15,7 @@ const page = () => {
       <ProjectSection/>
       <TapeSection/>
       <AboutSection/>
+      <ContactSection/>
     </div>
   )
 }
