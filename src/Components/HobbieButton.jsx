7b6a93823fed7d2ac50b0e5b4289d2865e560608
Abhishek jaiswal <abhijaiswal7479@gmail.com>
2026-02-01 22:24:04 +0530

    New Featute bug fixed- drag button

--- src/Components/HobbieButton.jsx
+++ src/Components/HobbieButton.jsx
@@ -1,12 +1,17 @@
 "use client"
 
 import React from 'react'
-import { motion } from 'framer-motion'
+import { motion, useMotionValue } from 'framer-motion'
 const HobbieButton = ({hobbie,icon,constraintsRef}) => {
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
   return (
     <motion.div
+    style={{x,y,touchAction:"none"}}
     drag
     dragConstraints={constraintsRef}
+    dragElastic={0}
+    dragMomentum={false}
      className='  text-black cursor-pointer  font-bold bg-linear-to-r from-blue-400 to-emerald-400 flex px-3 md:px-6 py-2 h-10 rounded-full ' >
         <div className='flex gap-2' >
             <div  >
