6f7709eaae791db390fd832a6bed15a1680f09fb
Abhishek jaiswal <abhijaiswal7479@gmail.com>
2026-02-02 12:17:51 +0530

    Tape section : added translate for rotation

--- src/Sections/TapeSection.js
+++ src/Sections/TapeSection.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { BsStars } from "react-icons/bs";
 import { data } from '@/Components/TapeData';
 const TapeSection = () => {
   return (
-    <div className='mt-24 mb-16 overflow-x-clip' >
-        <div className='-rotate-2 w-[99.99%]' >
-        <div className='flex whitespace-nowrap bg-linear-to-r from-green-400 to-blue-400 lg:p-4 p-2 lg:gap-6 gap-2  items-center ' >
-            {
+    <div className='mt-24 mb-16 w-full overflow-x-clip ' >
+        <div className=' -rotate-12 md:-rotate-2 w-[99.99%] bg-linear-to-r from-green-400 to-blue-400' >
+        <div className='flex w-full whitespace-nowrap pr-2  lg:p-4 p-2 lg:gap-6 gap-2  items-center ,md:animate-trans-x animate-trans-x-fast ' >
+            {[...new Array(4)].fill(0).map((_, index) =>
+            <Fragment key={index} >
+                {
                 data.items.map((item,index)=>{
                     return(
                         <div 
@@ -16,6 +18,8 @@ const TapeSection = () => {
                     )
                 })
             }
+            </Fragment>
+             )}
         </div>
         </div>
     </div>
