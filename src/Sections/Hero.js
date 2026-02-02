44d878ebf63df3313fa05a9d516980688e663056
Abhishek jaiswal <abhijaiswal7479@gmail.com>
2026-02-02 09:37:52 +0530

    bug fixed- stars rottion

--- src/Sections/Hero.js
+++ src/Sections/Hero.js
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import { FaArrowDown } from "react-icons/fa";
 import Ring from '@/Components/Ring';
 import { LiaStarSolid } from "react-icons/lia";
+import Link from 'next/link';
 
 
 
@@ -15,11 +16,11 @@ const Hero = () => {
 
       <div className='absolute inset-0  md:-top-[25rem] -z-10  ' >
         <div
-          className={`border border-emerald-500/20 shadow-[0_0_80px_inset] shadow-emerald-300/10 absolute top-[54%] left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full flex justify-start items-start size-[20rem] md:size-[40rem] animate-spin-md `} 
+          className={`border border-emerald-500/20 shadow-[0_0_80px_inset] shadow-emerald-300/10 absolute top-1/2 md:top-[54%] left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full flex justify-start items-start size-[30rem] md:size-[40rem] animate-spin-slow `} 
           >
             
             <LiaStarSolid 
-            className='text-emerald-400/20 text-xl absolute top-120 left-1/2 translate-x-[20rem] '
+            className='text-emerald-400/20 text-xs absolute top-120 left-1/2 translate-x-[20rem] '
             />
             <LiaStarSolid 
             className='text-emerald-400/80 text-2xl absolute top-30 left-60 translate-x-[20rem] '
@@ -28,30 +29,49 @@ const Hero = () => {
             className='text-emerald-400/80 text-3xl absolute top-0 left-0 translate-x-[20rem] '
             />
             <LiaStarSolid 
-            className='text-emerald-400/80 text-2xl absolute bottom-0 -left-40 translate-x-[20rem] '
+            className='text-emerald-400/80 text-xl absolute md:hidden top-20 left-10 translate-x-[20rem] '
             />
+            <LiaStarSolid 
+            className='text-emerald-400/80 text-xl absolute md:hidden bottom-10 left-0 translate-x-[20rem] '
+            />
+            <LiaStarSolid 
+            className='text-emerald-400/80 text-xs absolute -top-10 -left-40 translate-x-[20rem] '
+            />
+            <LiaStarSolid 
+            className='text-emerald-400/80 text-2xl absolute bottom-0 left-20 translate-x-[20rem] '
+            />
+            <LiaStarSolid 
+            className='text-emerald-400/80 text-2xl absolute bottom-40 left-40 translate-x-[20rem] '
+            />
+            
 
             
 
           </div>
         <div
-          className={`border border-emerald-500/20 shadow-[0_0_80px_inset] shadow-emerald-300/10 absolute top-[54%] left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full flex justify-start items-start size-[30rem] md:size-[60rem] animate-spin-slow `} 
+          className={`border border-emerald-500/20 shadow-[0_0_80px_inset] shadow-emerald-300/10 absolute top-1/2 md:top-[54%] left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full flex justify-start items-start size-[40rem] md:size-[60rem] animate-spin-md `} 
           >
             <LiaStarSolid 
-            className='text-emerald-400/50 text-4xl absolute bottom-100 -left-100 translate-x-[30rem] '
+            className='text-emerald-400/50 text-xl absolute bottom-100 -left-100 translate-x-[30rem] '
+            />
+            <LiaStarSolid 
+            className='text-emerald-400/50 text-xs absolute bottom-150 -left-90 translate-x-[30rem] '
             />
             <LiaStarSolid 
             className='text-emerald-400/30 text-2xl absolute top-30 -left-80 translate-x-[30rem] '
             />
             
             <LiaStarSolid 
-            className='text-emerald-400/80 text-2xl absolute top-50 right-120 translate-x-[30rem] '
+            className='text-emerald-400/80 text-2xl absolute top-120 right-120 translate-x-[30rem] '
+            />
+            <LiaStarSolid 
+            className='text-emerald-400/30 text-xl absolute -top-10 -left-30 translate-x-[30rem] '
             />
 
             
           </div>
         <div
-          className={`border border-emerald-500/20 shadow-[0_0_80px_inset] shadow-emerald-300/10 absolute top-[54%] left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full flex justify-start items-start size-[40rem] md:size-[80rem] animate-spin-slow `} 
+          className={`border border-emerald-500/20 shadow-[0_0_80px_inset] shadow-emerald-300/10 absolute top-1/2 md:top-[54%] left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full flex justify-start items-start size-[50rem] md:size-[80rem] animate-spin-slow `} 
           >
             <LiaStarSolid 
             className='text-emerald-400/50 text-4xl absolute bottom-200 -left-150 translate-x-[40rem] '
@@ -63,10 +83,16 @@ const Hero = () => {
             <LiaStarSolid 
             className='text-emerald-400/80 text-2xl absolute bottom-80 -left-120 translate-x-[40rem] '
             />
+            <LiaStarSolid 
+            className='text-emerald-400/80 text-xl absolute bottom-30 -left-80 translate-x-[40rem] '
+            />
+            <LiaStarSolid 
+            className='text-emerald-400/80 text-xl absolute bottom-40 -left-120 translate-x-[40rem] '
+            />
 
           </div>
         <div
-          className={`border border-emerald-500/20 shadow-[0_0_80px_inset] shadow-emerald-300/10 absolute top-[54%] left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full flex justify-start items-start size-[50rem] md:size-[100rem] max-md:hidden animate-spin-slow `} 
+          className={`border border-emerald-500/20 shadow-[0_0_80px_inset] shadow-emerald-300/10 absolute top-1/2 md:top-[54%] left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full flex justify-start items-start size-[60rem] md:size-[100rem] max-md:hidden animate-spin-slow `} 
           >
             <LiaStarSolid 
             className='text-emerald-400/50 text-4xl absolute bottom-50 left-100 translate-x-[50rem] '
@@ -77,9 +103,27 @@ const Hero = () => {
             <LiaStarSolid 
             className='text-emerald-400/30 text-2xl absolute -top-50 right-10 translate-y-[50rem] '
             />
+            <LiaStarSolid 
+            className='text-emerald-400/30 text-3xl absolute -top-100 right-20 translate-y-[50rem] '
+            />
+            <LiaStarSolid 
+            className='text-emerald-400/30 text-3xl absolute -top-100 right-40 translate-y-[50rem] '
+            />
+            <LiaStarSolid 
+            className='text-emerald-400/30 text-xl absolute top-30 right-20 translate-y-[50rem] '
+            />
+            <LiaStarSolid 
+            className='text-emerald-400/70 text-2xl absolute top-40 right-90 translate-y-[50rem] '
+            />
             
             <LiaStarSolid 
             className='text-emerald-400/80 text-2xl absolute bottom-150 -left-180 translate-x-[50rem] '
+            />
+            <LiaStarSolid 
+            className='text-emerald-400/80 text-2xl absolute bottom-50 -left-120 translate-x-[50rem] '
+            />
+            <LiaStarSolid 
+            className='text-emerald-400/80 text-2xl absolute bottom-280 -left-170 translate-x-[50rem] '
             />
 
           </div>
@@ -111,15 +155,13 @@ const Hero = () => {
               I specialize in transforming design concepts into clean, scalable, and high-quality code. With a strong focus on performance, responsiveness, and accessibility, 
             </div>
             <div className=' mt-8 flex gap-4 text-[1.2rem] max-md:text-[0.9rem] font-semibold ' >
-              <button
+              <Link href="#projects"
               className='border-2 border-gray-500 px-4 py-2 rounded-lg flex justify-center items-center
                '  >
                 Explore My Work
-                  {/* <Image src={downArrow} alt="Down Arrow" width={20} height={20} className='inline-block justify-center items-center font-bold' style={{filter: 'brightness(0) invert(1)'}} /> */}
-
                   <FaArrowDown className='text-white flex justify-center items-center ml-2' />
 
-                </button>
+                </Link>
               <button 
               className='border-2 hover:border-gray-500 px-4 py-1.5 rounded-lg bg-gray-100 text-black  '  >
                <span className='mr-1' >👋</span> Let's Connect</button>
